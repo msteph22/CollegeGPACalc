f09238dc9f2c25fac9312a6773803deb62875557
msteph22 <71839196+msteph22@users.noreply.github.com>
2020-10-05 14:41:02 -0500

    Rough UI
    Added a rough draft UI. Added feature that resets the disappeared text and combo boxes to default.

--- College_GPA_Calculator/College_GPA_Calculator/Form1.Designer.vb
+++ College_GPA_Calculator/College_GPA_Calculator/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,12 +20,225 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.CBGrade1 = New System.Windows.Forms.ComboBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.CBGrade2 = New System.Windows.Forms.ComboBox()
+        Me.CBGrade3 = New System.Windows.Forms.ComboBox()
+        Me.CBGrade4 = New System.Windows.Forms.ComboBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.CBGrade5 = New System.Windows.Forms.ComboBox()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.CBGrade6 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'CBGrade1
+        '
+        Me.CBGrade1.FormattingEnabled = True
+        Me.CBGrade1.Items.AddRange(New Object() {"A", "B", "C", "D", "F"})
+        Me.CBGrade1.Location = New System.Drawing.Point(176, 124)
+        Me.CBGrade1.Name = "CBGrade1"
+        Me.CBGrade1.Size = New System.Drawing.Size(99, 21)
+        Me.CBGrade1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(456, 190)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(99, 31)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Calculate"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(316, 125)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(90, 20)
+        Me.TextBox1.TabIndex = 2
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label1
+        '
+        Me.Label1.Location = New System.Drawing.Point(316, 99)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(90, 23)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Credit Hours"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label2
+        '
+        Me.Label2.Location = New System.Drawing.Point(176, 98)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(99, 23)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Grade"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'CBGrade2
+        '
+        Me.CBGrade2.FormattingEnabled = True
+        Me.CBGrade2.Items.AddRange(New Object() {"A", "B", "C", "D", "F"})
+        Me.CBGrade2.Location = New System.Drawing.Point(176, 162)
+        Me.CBGrade2.Name = "CBGrade2"
+        Me.CBGrade2.Size = New System.Drawing.Size(99, 21)
+        Me.CBGrade2.TabIndex = 5
+        '
+        'CBGrade3
+        '
+        Me.CBGrade3.FormattingEnabled = True
+        Me.CBGrade3.Items.AddRange(New Object() {"A", "B", "C", "D", "F"})
+        Me.CBGrade3.Location = New System.Drawing.Point(176, 200)
+        Me.CBGrade3.Name = "CBGrade3"
+        Me.CBGrade3.Size = New System.Drawing.Size(99, 21)
+        Me.CBGrade3.TabIndex = 6
+        '
+        'CBGrade4
+        '
+        Me.CBGrade4.FormattingEnabled = True
+        Me.CBGrade4.Items.AddRange(New Object() {"A", "B", "C", "D", "F"})
+        Me.CBGrade4.Location = New System.Drawing.Point(176, 238)
+        Me.CBGrade4.Name = "CBGrade4"
+        Me.CBGrade4.Size = New System.Drawing.Size(99, 21)
+        Me.CBGrade4.TabIndex = 7
+        Me.CBGrade4.Visible = False
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(316, 163)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(90, 20)
+        Me.TextBox2.TabIndex = 8
+        Me.TextBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(316, 201)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(90, 20)
+        Me.TextBox3.TabIndex = 9
+        Me.TextBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(316, 239)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(90, 20)
+        Me.TextBox4.TabIndex = 10
+        Me.TextBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        Me.TextBox4.Visible = False
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(316, 277)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(90, 20)
+        Me.TextBox5.TabIndex = 14
+        Me.TextBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        Me.TextBox5.Visible = False
+        '
+        'CBGrade5
+        '
+        Me.CBGrade5.FormattingEnabled = True
+        Me.CBGrade5.Items.AddRange(New Object() {"A", "B", "C", "D", "F"})
+        Me.CBGrade5.Location = New System.Drawing.Point(176, 276)
+        Me.CBGrade5.Name = "CBGrade5"
+        Me.CBGrade5.Size = New System.Drawing.Size(99, 21)
+        Me.CBGrade5.TabIndex = 13
+        Me.CBGrade5.Visible = False
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(316, 315)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(90, 20)
+        Me.TextBox6.TabIndex = 17
+        Me.TextBox6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        Me.TextBox6.Visible = False
+        '
+        'CBGrade6
+        '
+        Me.CBGrade6.FormattingEnabled = True
+        Me.CBGrade6.Items.AddRange(New Object() {"A", "B", "C", "D", "F"})
+        Me.CBGrade6.Location = New System.Drawing.Point(176, 314)
+        Me.CBGrade6.Name = "CBGrade6"
+        Me.CBGrade6.Size = New System.Drawing.Size(99, 21)
+        Me.CBGrade6.TabIndex = 16
+        Me.CBGrade6.Visible = False
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"1", "2", "3", "4", "5", "6"})
+        Me.ComboBox1.Location = New System.Drawing.Point(456, 146)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(115, 21)
+        Me.ComboBox1.TabIndex = 19
+        Me.ComboBox1.Text = "Number of Classes"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(456, 239)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(99, 31)
+        Me.Button2.TabIndex = 20
+        Me.Button2.Text = "Clear"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.TextBox6)
+        Me.Controls.Add(Me.CBGrade6)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.CBGrade5)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.CBGrade4)
+        Me.Controls.Add(Me.CBGrade3)
+        Me.Controls.Add(Me.CBGrade2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.CBGrade1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents CBGrade1 As ComboBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents CBGrade2 As ComboBox
+    Friend WithEvents CBGrade3 As ComboBox
+    Friend WithEvents CBGrade4 As ComboBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents CBGrade5 As ComboBox
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents CBGrade6 As ComboBox
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Button2 As Button
 End Class
--- College_GPA_Calculator/College_GPA_Calculator/Form1.vb
+++ College_GPA_Calculator/College_GPA_Calculator/Form1.vb
@@ -1,3 +1,94 @@
 ﻿Public Class Form1
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        Dim numofclasses As Integer
+        numofclasses = ComboBox1.Text
 
+        Select Case numofclasses
+            Case 1
+
+                CBGrade4.Visible = False
+                CBGrade4.ResetText()
+                TextBox4.Visible = False
+                TextBox4.Clear()
+
+                CBGrade5.Visible = False
+                CBGrade5.ResetText()
+                TextBox5.Visible = False
+                TextBox5.Clear()
+
+                CBGrade6.Visible = False
+                CBGrade6.ResetText()
+                TextBox6.Visible = False
+                TextBox6.Clear()
+            Case 2
+
+                CBGrade4.Visible = False
+                CBGrade4.ResetText()
+                TextBox4.Visible = False
+                TextBox4.Clear()
+
+                CBGrade5.Visible = False
+                CBGrade5.ResetText()
+                TextBox5.Visible = False
+                TextBox5.Clear()
+
+                CBGrade6.Visible = False
+                CBGrade6.ResetText()
+                TextBox6.Visible = False
+                TextBox6.Clear()
+            Case 3
+
+                CBGrade4.Visible = False
+                CBGrade4.ResetText()
+                TextBox4.Visible = False
+                TextBox4.Clear()
+
+                CBGrade5.Visible = False
+                CBGrade5.ResetText()
+                TextBox5.Visible = False
+                TextBox5.Clear()
+
+                CBGrade6.Visible = False
+                CBGrade6.ResetText()
+                TextBox6.Visible = False
+                TextBox6.Clear()
+            Case 4
+
+                CBGrade4.Visible = True
+                TextBox4.Visible = True
+
+                CBGrade5.Visible = False
+                CBGrade5.ResetText()
+                TextBox5.Visible = False
+                TextBox5.Clear()
+
+                CBGrade6.Visible = False
+                CBGrade6.ResetText()
+                TextBox6.Visible = False
+                TextBox6.Clear()
+            Case 5
+
+                CBGrade4.Visible = True
+                TextBox4.Visible = True
+
+                CBGrade5.Visible = True
+                TextBox5.Visible = True
+
+                CBGrade6.Visible = False
+                CBGrade6.ResetText()
+                TextBox6.Visible = False
+                TextBox6.Clear()
+            Case 6
+
+                CBGrade4.Visible = True
+                TextBox4.Visible = True
+
+                CBGrade5.Visible = True
+                TextBox5.Visible = True
+
+                CBGrade6.Visible = True
+                TextBox6.Visible = True
+        End Select
+
+    End Sub
 End Class
